b037077519f143083c74b6f7995de5f1092091f1
Victor Duprez <victor@ectorparking.com>
2019-07-18 11:03:17 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const items = ['Antoine', 'Rémy', 'Florian', 'Maéva', 'Gaëtan', 'Jalil', 'Victor', 'Klervi', 'Florent']
+
+function App () {
+const [search, setSearch] = React.useState('');
+const [results, setResults] = React.useState(null)
+
+React.useEffect(() => {
+  const results = items.filter(item => item.toLowerCase().includes(search.toLowerCase()));
+
+  setResults(results)
+}, [search])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={search} onChange={e => setSearch(e.currentTarget.value)} />
+      <div>
+      {results && results.map(
+        result => <div onClick={e => setSearch(result)}>{result}</div>
+      )}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
